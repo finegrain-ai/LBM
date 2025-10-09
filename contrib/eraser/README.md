6afc01e927edc85399cd56b41b10e8ab8df62724
Colle <piercus@users.noreply.github.com>
2025-10-09 10:46:33 +0100

    RORD eraser: scaffold (#1)

---
+++ contrib/eraser/README.md
@@ -0,0 +1,45 @@
+## LBM Eraser
+
+**Goal**: reproduce the object-removal result from the LBM paper (Section 4.1) using SD1.5.
+Non-official implementation.
+
+### Dataset
+
+Prepare a group of [webdataset](https://github.com/webdataset/webdataset) shards, organized like:
+
+```
+<id0>.after.jpg
+<id0>.before.jpg
+<id0>.mask.png
+<id1>.after.jpg
+<id1>.before.jpg
+<id1>.mask.png
+...
+```
+Where "before" is the image containing the object to remove, "mask" is the (fine) segmentation mask of the object in the before image, and "after" is the image without that object (ground truth).
+
+### Train
+
+To train the model on a single GPU machine, you can use the following command.
+Works on a single RTX 3090:
+
+```bash
+# Mock the SLURM env variables
+export SLURM_JOB_ID=0000 
+export SLURM_PROCID=0 
+export SLURM_ARRAY_TASK_ID=0
+export SLURM_NPROCS=1
+export SLURM_NODEID=0
+export SLURM_NNODES=1
+export SLURM_LOCALID=0 
+
+# Neptune (https://neptune.ai) settings. Use 'offline' mode to disable logging.
+export NEPTUNE_MODE=async
+export NEPTUNE_API_TOKEN="YOUR_API_TOKEN"
+
+# dataset
+export PATH_TO_TRAIN_TARS=...
+export PATH_TO_VAL_TARS=...
+
+python contrib/eraser/training/train_eraser.py contrib/eraser/training/config/eraser.yaml
+```
---
+++ contrib/eraser/training/train_eraser.py
@@ -0,0 +1,558 @@
+import datetime
+import logging
+import os
+import random
+import re
+import shutil
+from typing import List, Optional, Tuple
+
+import braceexpand
+import fire
+import torch
+import yaml
+from diffusers import FlowMatchEulerDiscreteScheduler, StableDiffusionPipeline
+from diffusers.models import UNet2DConditionModel
+from diffusers.models.attention import BasicTransformerBlock
+from diffusers.models.resnet import ResnetBlock2D
+from pytorch_lightning import Trainer, loggers
+from pytorch_lightning.callbacks import LearningRateMonitor, ModelCheckpoint
+from pytorch_lightning.strategies import FSDPStrategy
+from torch.distributed.fsdp.wrap import ModuleWrapPolicy
+from torchvision.transforms import InterpolationMode
+
+from lbm.data.datasets import DataModule, DataModuleConfig
+from lbm.data.filters import KeyFilter, KeyFilterConfig
+from lbm.data.mappers import (
+    KeyRenameMapper,
+    KeyRenameMapperConfig,
+    MapperWrapper,
+    RescaleMapper,
+    RescaleMapperConfig,
+    TorchvisionMapper,
+    TorchvisionMapperConfig,
+)
+from lbm.models.embedders import (
+    ConditionerWrapper,
+    LatentsConcatEmbedder,
+    LatentsConcatEmbedderConfig,
+)
+from lbm.models.lbm import LBMConfig, LBMModel
+from lbm.models.unets import DiffusersUNet2DCondWrapper
+from lbm.models.vae import AutoencoderKLDiffusers, AutoencoderKLDiffusersConfig
+from lbm.trainer import TrainingConfig, TrainingPipeline
+from lbm.trainer.loggers import NeptuneLogger
+from lbm.trainer.utils import StateDictAdapter
+
+
+def get_model(
+    backbone_signature: str = "stable-diffusion-v1-5/stable-diffusion-v1-5",
+    vae_num_channels: int = 4,
+    unet_input_channels: int = 4,
+    timestep_sampling: str = "log_normal",
+    selected_timesteps: Optional[List[float]] = None,
+    prob: Optional[List[float]] = None,
+    conditioning_images_keys: Optional[List[str]] = [],
+    conditioning_masks_keys: Optional[List[str]] = [],
+    source_key: str = "before",
+    target_key: str = "after",
+    mask_key: str = "mask",
+    bridge_noise_sigma: float = 0.0,
+    logit_mean: float = 0.0,
+    logit_std: float = 1.0,
+    pixel_loss_type: str = "lpips",
+    latent_loss_type: str = "l2",
+    latent_loss_weight: float = 1.0,
+    pixel_loss_weight: float = 0.0,
+):
+
+    conditioners = []
+
+    # Load pretrained model as base
+    pipe = StableDiffusionPipeline.from_pretrained(
+        backbone_signature,
+        torch_dtype=torch.bfloat16,
+    )
+
+    ### MMMDiT ###
+    # Get Architecture
+    denoiser = DiffusersUNet2DCondWrapper(
+        in_channels=unet_input_channels,  # Add downsampled_image
+        out_channels=vae_num_channels,
+        center_input_sample=False,
+        flip_sin_to_cos=True,
+        freq_shift=0,
+        down_block_types=[
+            # SD15 specific
+            "CrossAttnDownBlock2D",
+            "CrossAttnDownBlock2D",
+            "CrossAttnDownBlock2D",
+            "DownBlock2D"
+        ],
+        mid_block_type="UNetMidBlock2DCrossAttn",
+        up_block_types=[
+            # SD15 specific
+            "UpBlock2D",
+            "CrossAttnUpBlock2D",
+            "CrossAttnUpBlock2D",
+            "CrossAttnUpBlock2D"
+        ],
+        only_cross_attention=False,
+        block_out_channels=[320, 640, 1280, 1280], # match SD1.5
+        layers_per_block=2,
+        downsample_padding=1,
+        mid_block_scale_factor=1,
+        dropout=0.0,
+        act_fn="silu",
+        norm_num_groups=32,
+        norm_eps=1e-05,
+        cross_attention_dim=[320, 640, 1280, 1280], # match SD1.5 = block_out_channels
+        transformer_layers_per_block=1,
+        reverse_transformer_layers_per_block=None,
+        encoder_hid_dim=None,
+        encoder_hid_dim_type=None,
+        attention_head_dim=8,
+        num_attention_heads=None,
+        dual_cross_attention=False,
+        use_linear_projection=False,
+        class_embed_type=None,
+        addition_embed_type=None,
+        addition_time_embed_dim=None,
+        num_class_embeds=None,
+        upcast_attention=None,
+        resnet_time_scale_shift="default",
+        resnet_skip_time_act=False,
+        resnet_out_scale_factor=1.0,
+        time_embedding_type="positional",
+        time_embedding_dim=None,
+        time_embedding_act_fn=None,
+        timestep_post_act=None,
+        time_cond_proj_dim=None,
+        conv_in_kernel=3,
+        conv_out_kernel=3,
+        projection_class_embeddings_input_dim=None,
+        attention_type="default",
+        class_embeddings_concat=False,
+        mid_block_only_cross_attention=None,
+        cross_attention_norm=None,
+        addition_embed_type_num_heads=64,
+    ).to(torch.bfloat16)
+
+    state_dict = pipe.unet.state_dict()
+
+    denoise_state_dict = denoiser.state_dict()
+
+    # Adapt the shapes
+    state_dict_adapter = StateDictAdapter()
+    state_dict = state_dict_adapter(
+        model_state_dict=denoise_state_dict,
+        checkpoint_state_dict=state_dict,
+        regex_keys=[
+            r"class_embedding.linear_\d+.(weight|bias)",
+            r"conv_in.weight",
+            r"(down_blocks|up_blocks)\.\d+\.attentions\.\d+\.transformer_blocks\.\d+\.attn\d+\.(to_k|to_v)\.weight",
+            r"mid_block\.attentions\.\d+\.transformer_blocks\.\d+\.attn\d+\.(to_k|to_v)\.weight",
+        ],
+        strategy="zeros",
+    )
+    
+    denoiser.load_state_dict(state_dict, strict=True)
+
+    del pipe
+
+    if conditioning_images_keys != [] or conditioning_masks_keys != []:
+
+        latents_concat_embedder_config = LatentsConcatEmbedderConfig(
+            image_keys=conditioning_images_keys,
+            mask_keys=conditioning_masks_keys,
+        )
+        latent_concat_embedder = LatentsConcatEmbedder(latents_concat_embedder_config)
+        latent_concat_embedder.freeze()
+        conditioners.append(latent_concat_embedder)
+
+    # Wrap conditioners and set to device
+    conditioner = ConditionerWrapper(
+        conditioners=conditioners,
+    )
+
+    ## VAE ##
+    # Get VAE model
+    vae_config = AutoencoderKLDiffusersConfig(
+        version=backbone_signature,
+        subfolder="vae",
+    )
+    vae = AutoencoderKLDiffusers(vae_config)
+    vae.freeze()
+    vae.to(torch.bfloat16)
+
+    # LBM Config
+    config = LBMConfig(
+        ucg_keys=None,
+        source_key=source_key,
+        target_key=target_key,
+        mask_key=mask_key,
+        latent_loss_weight=latent_loss_weight,
+        latent_loss_type=latent_loss_type,
+        pixel_loss_type=pixel_loss_type,
+        pixel_loss_weight=pixel_loss_weight,
+        timestep_sampling=timestep_sampling,
+        logit_mean=logit_mean,
+        logit_std=logit_std,
+        selected_timesteps=selected_timesteps,
+        prob=prob,
+        bridge_noise_sigma=bridge_noise_sigma,
+    )
+
+    training_noise_scheduler = FlowMatchEulerDiscreteScheduler.from_pretrained(
+        backbone_signature,
+        subfolder="scheduler",
+    )
+    sampling_noise_scheduler = FlowMatchEulerDiscreteScheduler.from_pretrained(
+        backbone_signature,
+        subfolder="scheduler",
+    )
+
+    # LBM Model
+    model = LBMModel(
+        config,
+        denoiser=denoiser,
+        training_noise_scheduler=training_noise_scheduler,
+        sampling_noise_scheduler=sampling_noise_scheduler,
+        vae=vae,
+        conditioner=conditioner,
+    ).to(torch.bfloat16)
+
+    return model
+
+def get_filter_mappers(
+    image_size: Tuple[int, int] # (height, width)
+):
+    filters_mappers = [
+        KeyFilter(KeyFilterConfig(keys=["before.jpg", "after.jpg", "mask.png"], verbose=True)),
+        MapperWrapper(
+            [
+                KeyRenameMapper(
+                    KeyRenameMapperConfig(
+                        key_map={
+                            "before.jpg": "before",
+                            "after.jpg": "after",
+                            "mask.png": "mask",
+                        }
+                    )
+                ),
+                TorchvisionMapper(
+                    TorchvisionMapperConfig(
+                        key="before",
+                        transforms=["ToTensor", "Resize"],
+                        transforms_kwargs=[
+                            {},
+                            {
+                                "size": image_size,
+                                "interpolation": InterpolationMode.NEAREST_EXACT,
+                            },
+                        ],
+                    )
+                ),
+                TorchvisionMapper(
+                    TorchvisionMapperConfig(
+                        key="after",
+                        transforms=["ToTensor", "Resize"],
+                        transforms_kwargs=[
+                            {},
+                            {
+                                "size": image_size,
+                                "interpolation": InterpolationMode.NEAREST_EXACT,
+                            },
+                        ],
+                    )
+                ),
+                TorchvisionMapper(
+                    TorchvisionMapperConfig(
+                        key="mask",
+                        transforms=["ToTensor", "Resize", "Normalize"],
+                        transforms_kwargs=[
+                            {},
+                            {
+                                "size": image_size,
+                                "interpolation": InterpolationMode.NEAREST_EXACT,
+                            },
+                            {"mean": 0.0, "std": 1.0},
+                        ],
+                    )
+                ),
+                RescaleMapper(RescaleMapperConfig(key="before", verbose=True)),
+                RescaleMapper(RescaleMapperConfig(key="after", verbose=True)),
+            ],
+        ),
+    ]
+
+    return filters_mappers
+
+
+def get_data_module(
+    train_shards: List[str],
+    validation_shards: List[str],
+    batch_size: int,
+    image_size: Tuple[int, int] # (height, width)
+):
+
+    # TRAIN
+    train_filters_mappers = get_filter_mappers(image_size)
+
+    # unbrace urls
+    train_shards_path_or_urls_unbraced = []
+    for train_shards_path_or_url in train_shards:
+        train_shards_path_or_urls_unbraced.extend(
+            braceexpand.braceexpand(train_shards_path_or_url)
+        )
+
+    # shuffle shards
+    random.shuffle(train_shards_path_or_urls_unbraced)
+
+    # data config
+    data_config = DataModuleConfig(
+        shards_path_or_urls=train_shards_path_or_urls_unbraced,
+        decoder="pil",
+        shuffle_before_split_by_node_buffer_size=20,
+        shuffle_before_split_by_workers_buffer_size=20,
+        shuffle_before_filter_mappers_buffer_size=20,
+        shuffle_after_filter_mappers_buffer_size=20,
+        per_worker_batch_size=batch_size,
+        num_workers=min(10, len(train_shards_path_or_urls_unbraced)),
+    )
+
+    train_data_config = data_config
+
+    # VALIDATION
+    validation_filters_mappers = get_filter_mappers(image_size)
+
+    # unbrace urls
+    validation_shards_path_or_urls_unbraced = []
+    for validation_shards_path_or_url in validation_shards:
+        validation_shards_path_or_urls_unbraced.extend(
+            braceexpand.braceexpand(validation_shards_path_or_url)
+        )
+
+    data_config = DataModuleConfig(
+        shards_path_or_urls=validation_shards_path_or_urls_unbraced,
+        decoder="pil",
+        shuffle_before_split_by_node_buffer_size=10,
+        shuffle_before_split_by_workers_buffer_size=10,
+        shuffle_before_filter_mappers_buffer_size=10,
+        shuffle_after_filter_mappers_buffer_size=10,
+        per_worker_batch_size=batch_size,
+        num_workers=min(10, len(train_shards_path_or_urls_unbraced)),
+    )
+
+    validation_data_config = data_config
+
+    # data module
+    data_module = DataModule(
+        train_config=train_data_config,
+        train_filters_mappers=train_filters_mappers,
+        eval_config=validation_data_config,
+        eval_filters_mappers=validation_filters_mappers,
+    )
+
+    return data_module
+
+
+def main(
+    train_shards: List[str] = ["pipe:cat path/to/train/shards"],
+    validation_shards: List[str] = ["pipe:cat path/to/validation/shards"],
+    backbone_signature: str = "stable-diffusion-v1-5/stable-diffusion-v1-5",
+    vae_num_channels: int = 4,
+    unet_input_channels: int = 4,
+    source_key: str = "before",
+    target_key: str = "after",
+    mask_key: str = "mask",
+    neptune_project: str = "LBM-Eraser",
+    batch_size: int = 8,
+    num_steps: List[int] = [1, 2, 4],
+    learning_rate: float = 5e-5,
+    learning_rate_scheduler: str = None,
+    learning_rate_scheduler_kwargs: dict = {},
+    optimizer: str = "AdamW",
+    optimizer_kwargs: dict = {},
+    timestep_sampling: str = "uniform",
+    logit_mean: float = 0.0,
+    logit_std: float = 1.0,
+    pixel_loss_type: str = "lpips",
+    latent_loss_type: str = "l2",
+    latent_loss_weight: float = 1.0,
+    pixel_loss_weight: float = 0.0,
+    selected_timesteps: List[float] = None,
+    prob: List[float] = None,
+    conditioning_images_keys: Optional[List[str]] = [],
+    conditioning_masks_keys: Optional[List[str]] = [],
+    config_yaml: dict = None,
+    save_ckpt_path: str = "./checkpoints",
+    log_interval: int = 100,
+    resume_from_checkpoint: bool = True,
+    max_epochs: int = 100,
+    bridge_noise_sigma: float = 0.005,
+    save_interval: int = 1000,
+    path_config: str = None,
+    image_size: Tuple[int, int] | List[int] = (480, 640),  # (H, W)
+):
+    model = get_model(
+        backbone_signature=backbone_signature,
+        vae_num_channels=vae_num_channels,
+        unet_input_channels=unet_input_channels,
+        source_key=source_key,
+        target_key=target_key,
+        mask_key=mask_key,
+        timestep_sampling=timestep_sampling,
+        logit_mean=logit_mean,
+        logit_std=logit_std,
+        pixel_loss_type=pixel_loss_type,
+        latent_loss_type=latent_loss_type,
+        latent_loss_weight=latent_loss_weight,
+        pixel_loss_weight=pixel_loss_weight,
+        selected_timesteps=selected_timesteps,
+        prob=prob,
+        conditioning_images_keys=conditioning_images_keys,
+        conditioning_masks_keys=conditioning_masks_keys,
+        bridge_noise_sigma=bridge_noise_sigma,
+    )
+
+    if isinstance(image_size, list):
+        assert len(image_size) == 2, "image_size must be a tuple of (height, width)"
+        image_size = tuple(image_size)
+
+    data_module = get_data_module(
+        train_shards=train_shards,
+        validation_shards=validation_shards,
+        batch_size=batch_size,
+        image_size=image_size
+    )
+
+    train_parameters = ["denoiser.*"]
+
+    # Training Config
+    training_config = TrainingConfig(
+        learning_rate=learning_rate,
+        lr_scheduler_name=learning_rate_scheduler,
+        lr_scheduler_kwargs=learning_rate_scheduler_kwargs,
+        log_keys=["before", "after", "mask"],
+        trainable_params=train_parameters,
+        optimizer_name=optimizer,
+        optimizer_kwargs=optimizer_kwargs,
+        log_samples_model_kwargs={
+            "input_shape": None,
+            "num_steps": num_steps,
+        },
+    )
+    if (
+        os.path.exists(save_ckpt_path)
+        and resume_from_checkpoint
+        and "last.ckpt" in os.listdir(save_ckpt_path)
+    ):
+        start_ckpt = f"{save_ckpt_path}/last.ckpt"
+        print(f"Resuming from checkpoint: {start_ckpt}")
+
+    else:
+        start_ckpt = None
+
+    pipeline = TrainingPipeline(model=model, pipeline_config=training_config, verbose=True)
+
+    pipeline.save_hyperparameters(
+        {
+            f"embedder_{i}": embedder.config.to_dict()
+            for i, embedder in enumerate(model.conditioner.conditioners)
+        }
+    )
+
+    pipeline.save_hyperparameters(
+        {
+            "denoiser": model.denoiser.config,
+            "vae": model.vae.config.to_dict(),
+            "config_yaml": config_yaml,
+            "training": training_config.to_dict(),
+            "training_noise_scheduler": model.training_noise_scheduler.config,
+            "sampling_noise_scheduler": model.sampling_noise_scheduler.config,
+        }
+    )
+
+    training_signature = (
+        datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+        + "-LBM-Eraser"
+        + f"{os.environ['SLURM_JOB_ID']}"
+        + f"_{os.environ.get('SLURM_ARRAY_TASK_ID', 0)}"
+    )
+    dir_path = f"{save_ckpt_path}/logs/{training_signature}"
+    if os.environ["SLURM_PROCID"] == "0":
+        os.makedirs(dir_path, exist_ok=True)
+        if path_config is not None:
+            shutil.copy(path_config, f"{save_ckpt_path}/config.yaml")
+    run_name = training_signature
+
+    # Ignore parameters unused during training
+    ignore_states = []
+    for name, param in pipeline.model.named_parameters():
+        ignore = True
+        for regex in ["denoiser."]:
+            pattern = re.compile(regex)
+            if re.match(pattern, name):
+                ignore = False
+        if ignore:
+            ignore_states.append(param)
+
+    # FSDP Strategy
+    strategy = FSDPStrategy(
+        auto_wrap_policy=ModuleWrapPolicy(
+            [
+                UNet2DConditionModel,
+                BasicTransformerBlock,
+                ResnetBlock2D,
+                torch.nn.Conv2d,
+            ]
+        ),
+        activation_checkpointing_policy=ModuleWrapPolicy(
+            [
+                BasicTransformerBlock,
+                ResnetBlock2D,
+            ]
+        ),
+        sharding_strategy="SHARD_GRAD_OP",
+        ignored_states=ignore_states,
+    )
+
+    trainer = Trainer(
+        accelerator="gpu",
+        devices=int(os.environ["SLURM_NPROCS"]) // int(os.environ["SLURM_NNODES"]),
+        num_nodes=int(os.environ["SLURM_NNODES"]),
+        strategy=strategy,
+        default_root_dir="logs",
+        logger=loggers.NeptuneLogger(
+            project=neptune_project, name=run_name, log_model_checkpoints=False
+        ),
+        callbacks=[
+            NeptuneLogger(log_batch_freq=log_interval), 
+            LearningRateMonitor(logging_interval="step"),
+            ModelCheckpoint(
+                dirpath=save_ckpt_path,
+                every_n_train_steps=save_interval,
+                save_last=True,
+            ),
+        ],
+        num_sanity_val_steps=0,
+        precision="bf16-mixed",
+        limit_val_batches=2,
+        val_check_interval=1000,
+        max_epochs=max_epochs,
+    )
+
+    trainer.fit(pipeline, data_module, ckpt_path=start_ckpt)
+
+
+def main_from_config(path_config: str = None):
+    with open(path_config, "r") as file:
+        config = yaml.safe_load(file)
+    logging.info(
+        f"Running main with config: {yaml.dump(config, default_flow_style=False)}"
+    )
+    main(**config, config_yaml=config, path_config=path_config)
+
+
+if __name__ == "__main__":
+    fire.Fire(main_from_config)
