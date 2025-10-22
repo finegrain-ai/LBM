1c92117bc255f0452a158e058a971eb1d959d30f
Colle <piercus@users.noreply.github.com>
2025-10-22 14:49:28 +0200

    Update src/lbm/contrib/data/masking/lama.py
    Co-authored-by: Cédric Deltheil <355031+deltheil@users.noreply.github.com>

--- src/lbm/contrib/data/masking/lama.py
+++ src/lbm/contrib/data/masking/lama.py
@@ -1,4 +1,17 @@
-# Copied from https://github.com/advimman/lama/blob/786f5936b27fb3dacd2b1ad799e4de968ea697e7/saicinpainting/training/data/masks.py
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# Adapted from https://github.com/advimman/lama/blob/786f5936b27fb3dacd2b1ad799e4de968ea697e7/saicinpainting/training/data/masks.py
+# Changes:
 # - Removed segmentation-based mask generation because 
 #    A/ it's not used in official LaMa training
 #    B/ it requires detectron2 mask_evaluation.py and other dependencies to work
