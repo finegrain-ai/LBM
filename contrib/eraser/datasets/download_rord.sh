03332591affdc7136d45ed326e40b0f13882a0a4
Pierre Colle <piercus@gmail.com>
2025-10-27 13:36:40 +0000

    RORD-shuffled

--- contrib/eraser/datasets/download_rord.sh
+++ contrib/eraser/datasets/download_rord.sh
@@ -3,9 +3,9 @@ set -euo pipefail
 
 mkdir -p data/presencesw
 # Unofficial mirror, see https://huggingface.co/datasets/presencesw/RORD
-hf download presencesw/RORD --repo-type dataset --include "zip/*" --local-dir ./data/presencesw/
+hf download presencesw/RORD --repo-type dataset --include "zip/*" --local-dir ./data/presencesw/RORD
 
 pushd data/presencesw/RORD/zip
 # We must use 7z here with multithreading enabled
-7z x RORD.zip -mmt=on -o ../../RORD
+7z x RORD.zip -mmt=on -o../../RORD
 popd
